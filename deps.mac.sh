9cf1cca8bf69fdf9b9470623c6c0f42cb948fce8
Daniel Heffner <22029918+dheffx@users.noreply.github.com>
2019-12-15 11:24:53 -0500

    add dotnet and java

---
+++ deps.mac.sh
@@ -0,0 +1,11 @@
+## jsii-pacmak deps
+
+# python
+pip3 install setuptools twine wheel
+
+# java
+brew install maven
+
+# dotnet
+echo "dotnet must be downloaded from ms website"
+open https://dotnet.microsoft.com/download
